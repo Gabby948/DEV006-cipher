bc09fdefffa46c8a103e77865d0a1fa5823341d8
Gabriela Pintado V <125084134+Gabby948@users.noreply.github.com>
2023-03-28 12:49:13 -0500

    Update5

--- src/cipher.js
+++ src/cipher.js
@@ -1,38 +1,53 @@
 
+ export default cipher;
+
+ const cipher = {
+  encode: encode,
+  decode: decode }
+
+ 
+
+  encode: (message2, displacement) => {
+  let encodeResult = "";
+  
+    for (let i=0; i<message2.length; i++){
+      let character = message2 [i];
+      if (character.match (/[A-Z]/i)){
+        let message2Ascii = (message2.charCodeAt(i)-65+ parseInt (displacement)) %26+65;
+        result += String.fromCharCode (messageAscii);        
+      }
+      else if(message2 === null || displacement === 0) {
+        throw new TypeError
+
+        }
+       
+      else{
+        encodeMessage2 += character;
+      }             
+    }
+    return encodeMessage2;
+  },
+
+  decode: (message2, displacement) => {
+    let decodeResult = "";
+
+    for (let i = 0; i < message2.length; i++) {
+      let character2 = message2[i];
+      if (character2.match(/[A-Z]/i)) {
+        let message2Ascii2 = (message2.charCodeAt(i) + 65 - parseInt(displacement)) % 26 + 65;
+        decodeResult += String.fromCharCode(message2Ascii2);
+
+      }
+        else if(message2 === null || displacement === 0) {
+          throw new TypeError
+      } 
+
+      else{
+          decodeMessage += character2;
+      }
+    }
+      return decodeMessage; 
+ }
 
 
-export default cipher;
-
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const newAlphabet = "";
-function cipher(n){
-  for (let i = 0; i < alphabet.length; i++){
-    const offset = (i + n) % alphabet.length;
-    result += alphabet[offset];
-  }
-}
-function encode (message){
-  let result = "";
-  for (let i = 0; i < message.length; i++){
-    const index = alphabet.indexOf(message[i]);
-    result += newAlphabet[index];
-  }
-  return result;
-}
-function decode (message){
-  let result = ""; 
-  for (let i = 0; i < message.length; i++){
-    const index = newAlphabet.indexOf(message[i]);
-    result += alphabet[index];
-  }
-  return result;
-}
-
-
-
-
-
-
-
-
-
+ 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@ import cipher from './cipher.js';
 
 
 
-
-
 document.getElementById("message").addEventListener("keyup", function () {
   this.value = this.value.toUpperCase();
 });
 
-document.getElementById("cipher").addEventListener("click", function () {
+document.getElementById("encode").addEventListener("click", function () {
   
 
   const text = document.getElementById("message").value;
   const displacement = document.getElementById("displacement").value;
   cipher(text, displacement);
 });
-document.getElementById("decipher").addEventListener("click", function () {
+document.getElementById("decode").addEventListener("click", function () {
   const text = document.getElementById("message").value;
   const displacement = document.getElementById("displacement").value;
   cipher(text, displacement);
