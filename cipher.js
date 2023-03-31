b4dc2cbaabce292f4ec9df4432edab21ad63a901
Gabriela Pintado V <125084134+Gabby948@users.noreply.github.com>
2023-03-31 09:16:51 -0500

    Updates

--- cipher.js
+++ cipher.js
@@ -1,6 +1,52 @@
+
 const cipher = {
-  // ...
+
+
+
+  encode: (offsetNumber, message) => { //verificar si son string u objeto.
+       
+    //if(message === null || offsetNumber === 0) {
+    //throw new TypeError }
+    if (offsetNumber === null || message === ""|| offsetNumber === 0) {
+      throw new TypeError("Solo letras mayusculas")
+    }
+
+    let encodeMessage = "";
+    for (let i = 0; i < message.length; i++) {
+      const character = message[i].charCodeAt();
+    
+      if (character >= 65 && character <= 90) {
+        const messageAscii = (character - 65 + parseInt(offsetNumber)) % 26 + 65;
+        encodeMessage += String.fromCharCode(messageAscii);
+      }
+
+      else {
+        encodeMessage += character;
+      }
+    }
+    return encodeMessage;
+  },
+
+  decode: (offsetNumber, message) => {
+
+    if (offsetNumber === null || message === ""|| offsetNumber === 0) {
+      throw new TypeError("Solo letras mayusculas")}
+
+    let decodeMessage = "";
+
+    for (let i = 0; i < message.length; i++) {
+      const characterD = message[i].charCodeAt();
+      if (characterD >= 65 && characterD <= 90) {
+        const messageAsciiD = ((characterD - 65 - parseInt(offsetNumber)+26+26)% 26) + 65; //Doble modulo por la cantidad de vueltas que da en un abecedario//
+        //ABCDEFGHIJKLMNOPQRSTUVWXYZ//
+        decodeMessage += String.fromCharCode(messageAsciiD);
+      }
+      else {
+        decodeMessage += characterD;
+      }
+    }
+    return decodeMessage;
+  }
 };
 
 export default cipher;
-
--- index.js
+++ index.js
@@ -1,70 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-
-//window.addEventListener("load",start, true);
-
-//function start (){
-    
-    //document.getElementsById("message").addEventListener("keyup", function(){
-        //this.value = this.value.toUpperCase();
-    //},true);
-    
-//}
-    //document.getElementById("cipher").addEventListener("click", function(){
-       // let text = document.getElementById("message").value;
-        //let displacement = document.getElementById("displacement").value;
-        //document.getElementById ("message2").value = cipher2 (text,displacement);
-    //}, true);
-    //document.getElementById("decipher").addEventListener ("click", function(){
-        //let text = document.getElementById("message").value;
-        //let displacement = document.getElementById ("displacement").value;
-       // document.getElementById ("message2").value = decipher (text,displacement);
-    //},true);
 
 
-//function cifrar (text,displacement){
-    //let result = "";
-   // let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
+document.getElementById("message").addEventListener("keyup", function () {
+  this.value = this.value.toUpperCase();
+});
 
-    //displacement = (displacement % 26 + 26) % 26;
-
-    //if (text){
-        // for (let i = 0; i<text.length; i++){
-           // if (alphabet.indexOf (text [i])!=-1){
-                //let  posicion = ((alphabet.indexOf(text[i])+displacement)%26);
-                //result += alphabet[posicion];
-            //}
-            //else 
-               // result += text[i];
-         //}
-
-    //}
-    //return result;
-//}
-
-//function cipher2 (text, displacement){
-    //if (!text)
-        // return "";
-   // let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    //displacement = (displacement % 26 + 26)% 26;
-    //return text.replace (/[A-Z]/ig, c=> alphabet [(alphabet.indexOf(c)+displacement)%26]);
-
-//}
-//function decipher (text, displacement){
-    //if (!text)
-         //return "";
-    //let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    //displacement = (displacement % 26 - 26)% 26;
-
-    //return text.replace (/[A-Z]/ig, c=> alphabet [(alphabet.indexOf(c)-displacement)%26]);
-
-//}
-const alphabet = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
- function rot13 (str) {
+document.getElementById("encode").addEventListener("click", function () {
+  const encodeMessage = document.getElementById("message").value;
+  const offsetNumber = document.getElementById("offsetNumber").value;
+  document.getElementById ("message2").value = cipher.encode( offsetNumber, encodeMessage);
+});
 
 
- }
+document.getElementById("decode").addEventListener("click", function () {
+  const decodeMessage = document.getElementById("message").value;
+  const offsetNumberD = document.getElementById("offsetNumber").value;
+  document.getElementById ("message2").value = cipher.decode(offsetNumberD,decodeMessage);
+});
 
